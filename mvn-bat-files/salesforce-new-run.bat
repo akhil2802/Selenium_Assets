3d4eb4ad37a0c09123c3dc7c5282d8c7827b10e7
Akhil <akhil28021996.18@gmail.com>
2023-03-18 13:28:05 +0530

    Initial commit

---
+++ mvn-bat-files/salesforce-new-run.bat
@@ -0,0 +1,5 @@
+F:
+cd Work_Spaces\eclipse_workspace\crm_hybridframework
+call mvn compile
+call mvn test
+
---
+++ mvn-bat-files/salesforce-run.txt
@@ -0,0 +1,4 @@
+F:
+cd Work_Spaces\eclipse_workspace\pom_framework
+call mvn compile
+call mvn test
